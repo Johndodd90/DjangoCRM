bf35cf6d1344761a3600124d1c26177c32dc5211
John Dodd <johndodd90@gmail.com>
2020-04-16 20:18:34 +0100

    Lost password functionality added

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
@@ -16,4 +17,14 @@ urlpatterns = [
     path('create_order/<str:pk>/', views.createOrder, name='create_order'),
     path('update_order/<str:pk>/', views.updateOrder, name='update_order'),
     path('delete_order/<str:pk>/', views.deleteOrder, name='delete_order'),
+
+    # PATHS FOR PASSWORD RESETS
+    # path('reset_password/', auth_view.PasswordResetView.as_view(),
+    #     name="reset_password"),
+    # path('reset_password_sent/', auth_view.PasswordResetDoneView.as_view(),
+    #     name="password_reset_done"),
+    # path('reset/<uidb64>/<token>/', auth_view.PasswordResetConfirmView.as_view(),
+    #     name="password_reset_confirm"),
+    # path('reset_password_complete/', auth_view.PasswordResetCompleteView.as_view(),
+    #     name="password_reset_complete"),
 ]
